6bee369363b580058f1fa85fc16c59a4e2d23fdc
ethan <ethanmaregele@gmail.com>
2023-04-19 18:13:34 +0300

    views changed wiht doc string now

--- compulsary_task/blog/views.py
+++ compulsary_task/blog/views.py
@@ -8,16 +8,19 @@ def index(request):
     context = {'team_news':team_news}
     return render(request, 'top_blog.html', context)
 
+''' Go to the blog page and send from database the blog news table'''
 def blog(request):
     team_news = Post.objects.all()
     context = {'team_news':team_news}
     return render(request,'blog.html', context )
 
+''' got to the detail page and send the id of the team chosen'''
 def details(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
     context =  {'post':post}
     return render(request, 'detail.html', context)
 
+''' go to the table pagea nd send the clubs table form db'''
 def table(request):
     clubs = Clubs.objects.all().order_by('position').values()
     context = {'clubs':clubs}
